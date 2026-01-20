a4d4ec0403e37190883ea110bd813fe8e48a4a47
grimreaper0999 <katper0610003@gmail.com>
2026-01-21 00:25:55 +0100

    another forgotten import

--- app/checker/src/main/java/com/dockey/checker/services/CheckingService.java
+++ app/checker/src/main/java/com/dockey/checker/services/CheckingService.java
@@ -9,6 +9,7 @@ import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.client.Entity;
 
 
 import javax.enterprise.context.ApplicationScoped;
